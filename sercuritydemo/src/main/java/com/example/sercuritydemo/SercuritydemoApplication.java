f600db24c37e44f35d616ba9c7fbb403965180c9
James.Wang <why3046380@outlook.com>
2018-07-27 15:42:11 +0800

    添加了登陆验证框架

---
+++ sercuritydemo/src/main/java/com/example/sercuritydemo/SercuritydemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.sercuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercuritydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SercuritydemoApplication.class, args);
+    }
+}
