4be58367965e07e868f5c9c2197895676e13e366
James.Wang <why3046380@outlook.com>
2018-07-27 16:50:11 +0800

    添加了登陆验证框架

--- sercuritydemo/src/main/java/com/example/sercuritydemo/WebSecurityConfiguration.java
+++ sercuritydemo/src/main/java/com/example/sercuritydemo/WebSecurityConfiguration.java
@@ -1,27 +1,42 @@
 package com.example.sercuritydemo;
 
 import com.example.sercuritydemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService(){
         return  new UserService();
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().anyRequest().authenticated().and()
                 .formLogin().loginPage("/login")
-                .failureUrl("/login?error")
+                .failureUrl("/login?error").successForwardUrl("/")
                 .permitAll()
                 .and().logout().permitAll();
     }
-}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder());
+    }
 
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/config/**", "/css/**", "/fonts/**", "/img/**", "/js/**");
+    }
+
+}
