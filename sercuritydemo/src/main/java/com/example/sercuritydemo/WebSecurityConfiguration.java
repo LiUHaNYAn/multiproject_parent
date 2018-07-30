352c83acbe7e64c9c1989d760370db0ab95da43b
James.Wang <why3046380@outlook.com>
2018-07-30 13:05:32 +0800

    添加了登陆验证框架

--- sercuritydemo/src/main/java/com/example/sercuritydemo/WebSecurityConfiguration.java
+++ sercuritydemo/src/main/java/com/example/sercuritydemo/WebSecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +19,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService userDetailsService(){
         return  new UserService();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,8 +35,8 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService());
-        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder());
+        auth.inMemoryAuthentication().withUser("admin")
+                .password(passwordEncoder().encode("123456")).roles("USER");
     }
 
     @Override
